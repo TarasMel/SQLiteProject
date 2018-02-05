1c0ba7dd978f21d675f56dff2c5d5fa2366c671e
TarasMel <9I_KpeBeDko@ukr.net>
2018-02-05 07:46:48 +0200

    work with SQLite

---
+++ app/src/main/java/com/example/taras/sqliteproject/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.taras.sqliteproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+/**
+ * Created by Taras on 03.02.2018.
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd;
+    private Button btnViewData;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText_ID);
+        btnAdd = (Button) findViewById(R.id.btnAdd_ID);
+        btnViewData = (Button) findViewById(R.id.btnView_ID);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = editText.getText().toString();
+                if (editText.length() != 0) {
+                    AddData(newEntry);
+                    editText.setText("");
+                } else {
+                    toastMessage("You must put something in the text field!");
+                }
+
+            }
+        });
+
+        btnViewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    /**
+     customizable toast messages
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+}
